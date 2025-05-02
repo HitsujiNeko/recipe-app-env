be931af2c4f6618ba3d474ffaf762c5360b158b5
HitsujiNeko <takumid0419@outlookcom>
2025-05-02 02:35:40 +0000

    食材検索欄のデザインの改善

--- RecipeSuggestion_project/RecipeSuggestion_project/settings.py
+++ RecipeSuggestion_project/RecipeSuggestion_project/settings.py
@@ -162,7 +162,3 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
 }
-
-SUPERUSER_NAME = env("SUPERUSER_NAME")
-SUPERUSER_EMAIL = env("SUPERUSER_EMAIL")
-SUPERUSER_PASSWORD = env("SUPERUSER_PASSWORD")
